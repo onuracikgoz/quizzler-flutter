35175d6ad4e7fdaa77c5c28ffdca5d75e8e75827
onur <onuracikgoz28@gmail.com>
2019-11-18 15:31:20 +0300

    second update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'question_brain.dart';
 
 void main() => runApp(Quizzler());
 
@@ -25,6 +26,11 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Widget> scoreKeeper = [];
+  QuestionBrain questionBrain = new QuestionBrain() ;
+
+
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +43,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questionBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,7 +67,32 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                bool correctAnswer =
+                    questionBrain.getCorrentAnswer();
+                if (correctAnswer == true) {
+                  print('user got it right');
+                  setState(() {
+                    scoreKeeper.add(
+                      Icon(
+                        Icons.check,
+                        color: Colors.green,
+                        size: 30.0,
+                      ),
+                    );
+                  });
+                } else {
+                  print('user got it wrong');
+                  setState(() {
+                    scoreKeeper.add(
+                      Icon(
+                        Icons.close,
+                        color: Colors.red,
+                        size: 30.0,
+                      ),
+                    );
+                  });
+                }
+              questionBrain.nextQuestion();
               },
             ),
           ),
@@ -79,12 +110,39 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                bool correctAnswer =
+                    questionBrain.getCorrentAnswer();
+                if (correctAnswer == false) {
+                  print('user got it right');
+                  setState(() {
+                    scoreKeeper.add(
+                      Icon(
+                        Icons.check,
+                        color: Colors.green,
+                        size: 30.0,
+                      ),
+                    );
+                  });
+                } else {
+                  print('user got it wrong');
+                  setState(() {
+                    scoreKeeper.add(
+                      Icon(
+                        Icons.close,
+                        color: Colors.red,
+                        size: 30.0,
+                      ),
+                    );
+                  });
+                }
+                questionBrain.nextQuestion();
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        )
       ],
     );
   }
---
+++ lib/question.dart
@@ -0,0 +1,9 @@
+class Questions {
+  String questionText;
+  bool questionAnswer;
+
+  Questions(String questionText, bool questionAnswer ) {
+    this.questionText = questionText;
+    this.questionAnswer = questionAnswer;
+  }
+}
